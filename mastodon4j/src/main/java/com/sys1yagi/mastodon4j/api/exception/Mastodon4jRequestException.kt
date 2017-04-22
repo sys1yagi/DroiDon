2b2318443952472309cf1ef252b1492364922492
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-04-22 10:53:05 +0900

    Move mastodon4j to https://github.com/sys1yagi/mastodon4j

--- mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/exception/Mastodon4jRequestException.kt
+++
@@ -1,3 +0,0 @@
-package com.sys1yagi.mastodon4j.api.exception
-
-class Mastodon4jRequestException(message: String) : Exception(message)
--- mastodon4j/src/main/java/com/sys1yagi/mastodon4j/extension/GlobalFunctions.kt
+++
@@ -1,5 +0,0 @@
-package com.sys1yagi.mastodon4j.extension
-
-import com.google.gson.reflect.TypeToken
-
-inline fun <reified T> genericType() = object : TypeToken<T>() {}.type
