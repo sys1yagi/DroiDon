a12672152bb17bd2a79fab5e275d034b8bc5c162
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-04-29 08:09:44 +0900

    Fix test.

--- app/src/test/java/com/sys1yagi/mastodon/android/ui/navigation/home/timeline/TimelinePresenterTest.kt
+++ app/src/test/java/com/sys1yagi/mastodon/android/ui/navigation/home/timeline/TimelinePresenterTest.kt
@@ -2,11 +2,13 @@ package com.sys1yagi.mastodon.android.ui.navigation.home.timeline
 
 import com.google.gson.Gson
 import com.sys1yagi.kmockito.mock
+import com.sys1yagi.mastodon4j.api.Pageable
 import com.sys1yagi.mastodon4j.api.entity.Status
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.assertj.core.api.Assertions.assertThat
+
 @RunWith(RobolectricTestRunner::class)
 class TimelinePresenterTest {
 
@@ -16,14 +18,15 @@ class TimelinePresenterTest {
 
     @Test
     fun onTimeline() {
-        val presenter = TimelinePresenter(mock(), mock(), mock(), mock())
+        val presenter = TimelinePresenter(mock(), mock(), "mstdn.jp", mock(), mock())
 
         assertThat(presenter.viewModel.statuses).isEmpty()
 
-        presenter.onTimeline(listOf(status(1), status(2), status(3)))
+
+        presenter.onTimeline(Pageable<Status>(listOf(status(1), status(2), status(3)), null))
         assertThat(presenter.viewModel.statuses).hasSize(3)
 
-        presenter.onTimeline(listOf(status(4), status(5), status(1), status(2)))
+        presenter.onTimeline(Pageable<Status>(listOf(status(4), status(5), status(1), status(2)), null))
         assertThat(presenter.viewModel.statuses).hasSize(7)
     }
 }
