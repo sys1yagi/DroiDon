277eff485d48b7b98b8bf959896dd01b1f9ec264
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-04-30 21:40:31 +0900

    Show media preview.

--- app/src/main/java/com/sys1yagi/mastodon/android/extensions/ContextExtensions.kt
+++ app/src/main/java/com/sys1yagi/mastodon/android/extensions/ContextExtensions.kt
@@ -1,7 +1,9 @@
 package com.sys1yagi.mastodon.android.extensions
 
 import android.content.Context
+import android.support.annotation.DimenRes
 import com.sys1yagi.mastodon.android.DroiDonApplication
 
 fun Context.gson() = (applicationContext as DroiDonApplication).appComponent.provideGson()
 
+fun Context.getDimensionPixelSize(@DimenRes resId: Int) = resources.getDimensionPixelSize(resId)
--- app/src/main/java/com/sys1yagi/mastodon/android/view/TimelineAdapter.kt
+++ app/src/main/java/com/sys1yagi/mastodon/android/view/TimelineAdapter.kt
@@ -2,9 +2,15 @@ package com.sys1yagi.mastodon.android.view
 
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.facebook.drawee.view.SimpleDraweeView
+import com.sys1yagi.mastodon.android.R
 import com.sys1yagi.mastodon.android.databinding.ListItemStatusBinding
 import com.sys1yagi.mastodon.android.extensions.layoutInflator
 import com.sys1yagi.mastodon.android.data.model.TimelineStatus
+import com.sys1yagi.mastodon.android.extensions.getDimensionPixelSize
+import com.sys1yagi.mastodon.android.extensions.gone
+import com.sys1yagi.mastodon.android.extensions.visible
 
 typealias OnReplayClick = (TimelineStatus) -> Unit
 typealias OnReTweetClick = (TimelineStatus) -> Unit
@@ -32,6 +38,8 @@ class TimelineAdapter : RecyclerView.Adapter<TimelineAdapter.Holder>() {
             holder.binding.icon.setImageURI(it.avatar)
         } ?: holder.binding.icon.setImageURI(null as String?)
 
+        showMedia(holder.binding.mediaContainer, status)
+
         holder.apply {
             binding.status = status
             binding.replay.setOnClickListener {
@@ -45,6 +53,23 @@ class TimelineAdapter : RecyclerView.Adapter<TimelineAdapter.Holder>() {
             }
             binding.other.setOnClickListener {
                 onOtherClick(status)
+            }
+        }
+    }
+
+    fun showMedia(mediaContainer: LinearLayout, status: TimelineStatus) {
+        val context = mediaContainer.context
+        mediaContainer.removeAllViews()
+        if (status.entity.mediaAttachments.isEmpty()) {
+            mediaContainer.gone()
+        } else {
+            mediaContainer.visible()
+            status.entity.mediaAttachments.forEach {
+                val image = SimpleDraweeView(context)
+                image.setImageURI(it.previewUrl)
+                val params = LinearLayout.LayoutParams(0, context.getDimensionPixelSize(R.dimen.toot_media_preview_height))
+                params.weight = 1f
+                mediaContainer.addView(image, params)
             }
         }
     }
